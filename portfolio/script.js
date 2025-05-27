e63a671fb149d930db5d923d3fe2a0e5479a9ca9
Aadarsh Bhukele <aadarshbhukele2005@gmail.com>
2025-05-27 21:22:59 +0530

    Add files via upload
    improve the design and code.

--- portfolio/script.js
+++ portfolio/script.js
@@ -21,3 +21,12 @@ function setActiveStyle(color) {
         }
     })
 }
+/*-------------sidebar------------------*/
+function showSideBar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display ="flex";
+}
+function hideSideBar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display ="none";
+}
